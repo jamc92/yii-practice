562d510428e4d3eaa56fb9fd2ca931512ee135ed
jamc92 <javiermadrid19@hotmail.com>
2014-07-17 23:22:37 -0430

    Agregada funcion 'Eneable' & 'Disable' a los paises en lista

--- app/protected/views/countries/index.php
+++ app/protected/views/countries/index.php
@@ -14,16 +14,26 @@
 <!--En cada iteracion a $counties se obtieen una instancia del modelo-->
 <?php foreach($countries as $data):?>
 <!--Imprimiendo los nombres del registro y al status-->
-<h3><?php echo $data->name?> <small><?php echo $data->status==1?"Enabled":"Disabled";?></small></h3>
+<h3>
+    <?php echo $data->name?>
+    <!--Metodo para crear links en PHP-->
+    <a href="<?php echo $this->createUrl("enable",array("id"=>$data->id));?>">
+        <span class="label label-<?php echo $data->status==1?"info":"warning";?>">
+            <?php echo $data->status==1?"Enabled":"Disabled";?>
+        </span>
+    </a>
+</h3>
 
 <!--Creando link en cada registro y mostrando el ID-->
 <label class="badge badge-info"><?php echo $data->id?></label>
     <!--Parametros clave valor del metodo GET-->
-<?php echo CHtml::link("Update",array("update","id"=>$data->id)); ?>
- |
+<?php echo CHtml::link("Update",array("update","id"=>$data->id)); ?> |
 <!--Creando link para eliminar registro-->
-<?php echo CHtml::link("Delete",array("delete","id"=>$data->id),array("confirm"=>"Are you sure to delete this item?"));?>
-    <hr>
+<?php echo CHtml::link("Delete",array("delete","id"=>$data->id),array("confirm"=>"Are you sure to delete this item?"));?> |
+
+<!--Creando link para la vista detallada-->
+<?php echo CHtml::link("View",array("view","id"=>$data->id));?>
+<hr>
 
 
 <?php endforeach;?>
