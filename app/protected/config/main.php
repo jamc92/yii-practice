6c2eb51eaa415394929e4ca12b5a0527cfca7689
Javier Madrid <javiermadrid19@hotmail.com>
2014-07-18 23:26:51 -0430

    Update main.php

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -1,7 +1,7 @@
 <?php
 
 
-// uncomment the following to define a path alias
+// uncomment the following to define a path alia
 // Yii::setPathOfAlias('local','path/to/local-folder');
 
 // This is the main Web application configuration. Any writable
