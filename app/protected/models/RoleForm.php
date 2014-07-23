06b379adfc367ed077eb24ca147a0003bc28f8d9
jamc92 <javiermadrid19@hotmail.com>
2014-07-22 22:24:15 -0430

    Probando reglas de funcion para Rules y seteando mensajes de errores, metodos de validacion en los formularios

--- app/protected/models/RoleForm.php
+++ app/protected/models/RoleForm.php
@@ -13,10 +13,38 @@ class RoleForm extends CFormModel
 	public function rules()
 	{
 		return array(
-				#Definiendo los campos requeridos
-				array("name,type","required"),
-				#Regla de validacion para los datos que no tienen regla de validacion 'safe'
-				array("description","safe"),
+				#Definiendo los campos requeridos. Personalizar mensaje con 'message' {}refiere al atributo seleccionado
+				array("name,type","required","message"=>"Campo requerido {attribute}"),
+				
+				#Consultando al modelo(BD) si existen los datos
+				array("description","unique","attributeName"=>"username","className"=>"Users"),
+
+				#Seteando por filtros. Filtrando
+				#array("description","filter","filter"=>"strtolower"),
+
+				#[EJEMPLO] Validando la descripcion, que sea numerica y solo enteros con un mensaje personalizado
+				#array("description","numerical",'integerOnly'=>true,'message'=>'Pon un entero sucio!'),
+				
+				/*Regla de validacion para los datos que no tienen regla de validacion 'safe'
+				*Se pueden agregar mas parametros que se pueden setear en la clase, que tienen que ser propiedades de la clase
+				*en este caso MyValidator
+				*/
+				#array("description","ext.MyValidator","description"=>"this"),
 			);
 	}
+
+	#Creando validaciones personalizadas recibiendo 2 parametros. '$attribute y $params. Funcion de ejemplo
+
+	
+/*
+	public function validacion($attribute,$params)
+	{
+		#Se emplea la condicion
+		if($this->attribute=="test")
+		{
+			#Entonces se muestra el error mediante el metodo addError. Con el atributo  mensaje
+			$this->addError($attribute,"Mensaje");
+		}
+	}*/
+
 }
